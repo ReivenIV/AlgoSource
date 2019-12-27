63196e38f5f75242555e785b33b29be44e518f10
ziratsu <enzo.ustariz@hotmail.fr>
2019-12-27 11:07:52 +0100

    init

---
+++ Algos base/6. lePLusFrequent/app.js
@@ -0,0 +1,23 @@
+function lePlusFrequent(tab){
+
+}
+
+
+console.log(lePlusFrequent([3, 'a', 'a', 'a','b','b', 2, 3, 'a', 3, 'a', 2, 4, 9, 3])); // Le plus récurent est : a, il est compté 5 fois
+
+
+
+// -----------------  Énoncé ------------------- //
+
+// Créez un programme qui analyse l'élément qui est a le plus d'ocurrence dans un tableau
+
+// -----------------  Conseils ------------------- //
+
+// for loop
+// if
+
+
+
+
+
+
